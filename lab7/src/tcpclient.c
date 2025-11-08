9bf8480e3fbe88f0d3477e1214dfc2357bc8d059
Romashek <69082555+Romashek@users.noreply.github.com>
2025-11-08 07:56:18 +0000

    labba7

--- lab7/src/tcpclient.c
+++ lab7/src/tcpclient.c
@@ -7,48 +7,70 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#define BUFSIZE 100
 #define SADDR struct sockaddr
-#define SIZE sizeof(struct sockaddr_in)
 
 int main(int argc, char *argv[]) {
+  if (argc < 4) {
+    printf("Usage: %s <IP> <PORT> <BUFSIZE>\n", argv[0]);
+    exit(1);
+  }
+
+  char *ip = argv[1];
+  int port = atoi(argv[2]);
+  int bufsize = atoi(argv[3]);
+
+  // Проверка размера буфера
+  if (bufsize <= 0) {
+    printf("Error: BUFSIZE must be positive\n");
+    exit(1);
+  }
+
   int fd;
   int nread;
-  char buf[BUFSIZE];
-  struct sockaddr_in servaddr;
-  if (argc < 3) {
-    printf("Too few arguments \n");
+  char *buf = malloc(bufsize);  // Динамическое выделение памяти
+  if (buf == NULL) {
+    perror("malloc");
     exit(1);
   }
-
+  
+  struct sockaddr_in servaddr;
+  
   if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
     perror("socket creating");
+    free(buf);
     exit(1);
   }
 
-  memset(&servaddr, 0, SIZE);
+  memset(&servaddr, 0, sizeof(servaddr));
   servaddr.sin_family = AF_INET;
 
-  if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
+  if (inet_pton(AF_INET, ip, &servaddr.sin_addr) <= 0) {
     perror("bad address");
+    free(buf);
+    close(fd);
     exit(1);
   }
 
-  servaddr.sin_port = htons(atoi(argv[2]));
+  servaddr.sin_port = htons(port);
 
-  if (connect(fd, (SADDR *)&servaddr, SIZE) < 0) {
+  if (connect(fd, (SADDR *)&servaddr, sizeof(servaddr)) < 0) {
     perror("connect");
+    free(buf);
+    close(fd);
     exit(1);
   }
 
-  write(1, "Input message to send\n", 22);
-  while ((nread = read(0, buf, BUFSIZE)) > 0) {
+  printf("Connected to server. Type messages (Ctrl+D to exit):\n");
+  
+  while ((nread = read(0, buf, bufsize)) > 0) {
     if (write(fd, buf, nread) < 0) {
       perror("write");
-      exit(1);
+      break;
     }
   }
 
+  free(buf);
   close(fd);
-  exit(0);
+  printf("Disconnected\n");
+  return 0;
 }
--- lab7/src/tcpserver.c
+++ lab7/src/tcpserver.c
@@ -2,61 +2,101 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 
-#define SERV_PORT 10050
-#define BUFSIZE 100
 #define SADDR struct sockaddr
 
-int main() {
-  const size_t kSize = sizeof(struct sockaddr_in);
+int main(int argc, char *argv[]) {
+  if (argc < 3) {
+    printf("Usage: %s <PORT> <BUFSIZE>\n", argv[0]);
+    exit(1);
+  }
+
+  int port = atoi(argv[1]);
+  int bufsize = atoi(argv[2]);
+
+  // Проверка размера буфера
+  if (bufsize <= 0) {
+    printf("Error: BUFSIZE must be positive\n");
+    exit(1);
+  }
 
   int lfd, cfd;
   int nread;
-  char buf[BUFSIZE];
+  char *buf = malloc(bufsize);  // Динамическое выделение памяти
+  if (buf == NULL) {
+    perror("malloc");
+    exit(1);
+  }
+  
   struct sockaddr_in servaddr;
   struct sockaddr_in cliaddr;
 
   if ((lfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
     perror("socket");
+    free(buf);
     exit(1);
   }
 
-  memset(&servaddr, 0, kSize);
+  // Добавляем опцию переиспользования порта
+  int opt = 1;
+  if (setsockopt(lfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+    perror("setsockopt");
+    free(buf);
+    close(lfd);
+    exit(1);
+  }
+
+  memset(&servaddr, 0, sizeof(servaddr));
   servaddr.sin_family = AF_INET;
   servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  servaddr.sin_port = htons(SERV_PORT);
+  servaddr.sin_port = htons(port);
 
-  if (bind(lfd, (SADDR *)&servaddr, kSize) < 0) {
+  if (bind(lfd, (SADDR *)&servaddr, sizeof(servaddr)) < 0) {
     perror("bind");
+    free(buf);
+    close(lfd);
     exit(1);
   }
 
   if (listen(lfd, 5) < 0) {
     perror("listen");
+    free(buf);
+    close(lfd);
     exit(1);
   }
 
+  printf("TCP Server listening on port %d\n", port);
+
   while (1) {
-    unsigned int clilen = kSize;
+    unsigned int clilen = sizeof(cliaddr);
 
     if ((cfd = accept(lfd, (SADDR *)&cliaddr, &clilen)) < 0) {
       perror("accept");
-      exit(1);
+      continue;  // Продолжаем работать при ошибке accept
     }
-    printf("connection established\n");
+    
+    printf("Connection established from %s:%d\n", 
+           inet_ntoa(cliaddr.sin_addr), ntohs(cliaddr.sin_port));
 
-    while ((nread = read(cfd, buf, BUFSIZE)) > 0) {
-      write(1, &buf, nread);
+    while ((nread = read(cfd, buf, bufsize)) > 0) {
+      printf("Received: ");
+      fwrite(buf, 1, nread, stdout);  // Безопасный вывод
+      printf("\n");
     }
 
     if (nread == -1) {
       perror("read");
-      exit(1);
     }
+    
     close(cfd);
+    printf("Connection closed\n");
   }
+
+  free(buf);
+  close(lfd);
+  return 0;
 }
